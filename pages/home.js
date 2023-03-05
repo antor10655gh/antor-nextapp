272033181afd4fa6e86e331a6ef6083637b9d0cf
antor10655gh <creativefreelancerantor@gmail.com>
2023-03-05 17:55:27 +0600

    add some page

---
+++ pages/home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '@/styles/Home.module.css'
+
+export default function home() {
+  return (
+    <main className={styles.main}>
+      <h1>Home</h1>
+    </main>
+  )
+}
